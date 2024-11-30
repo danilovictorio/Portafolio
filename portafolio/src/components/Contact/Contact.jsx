3f7df52fe3cd7aaf552a6f1376b620b0192ce86d
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-11-30 13:31:40 +0100

    contacto

--- portafolio/src/components/Contact/Contact.jsx
+++ portafolio/src/components/Contact/Contact.jsx
@@ -7,24 +7,23 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contacto</h2>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <img src={getImageUrl("contact/gmail.png")} alt="Email icon" />
+          <a href="mailto:danilovictorio777@gmail.com">danilovictorio777@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
-            src={getImageUrl("contact/linkedinIcon.png")}
+            src={getImageUrl("contact/linkedin.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/danilo-victorio-85163a160/">linkedin.com/danilovictorio</a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <img src={getImageUrl("contact/github.png")} alt="Github icon" />
+          <a href="https://github.com/danilovictorio">github.com/danilovictorio</a>
         </li>
       </ul>
     </footer>
